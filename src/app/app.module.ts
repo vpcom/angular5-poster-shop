e899ad6a332326a2511703fa340ed3ae1fdf1a84
Vincent Perrin <vpcom01@gmail.com>
2018-01-04 23:02:45 +0100

    added most of main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { CoreComponent } from './core/core.component';
+import { CartComponent } from './cart/cart.component';
+import { BuyComponent } from './buy/buy.component';
+import { LogsComponent } from './logs/logs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    CoreComponent,
+    CartComponent,
+    BuyComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule
