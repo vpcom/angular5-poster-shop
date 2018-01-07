5f47557209fad9470d3687bfa466707b6ba71f6f
Vincent Perrin <vpcom01@gmail.com>
2018-01-07 15:21:14 +0100

    created variables, corrected dimensioning bugs between sidenav content, desktop and mobile view

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -15,6 +15,7 @@ export class MainComponent implements OnInit {
   expand = true;
   isMobile = false;
   subscription: Subscription;
+  firstTime = true;
 
   constructor(public media:ObservableMedia, private cartNotificationService: CartNotificationService) {
       // subscribe to toggle event for the cart panel
@@ -38,7 +39,10 @@ export class MainComponent implements OnInit {
   sidenavToggle() {
     if (this.isMobile) {
       let sidenavMob = document.getElementById('sidenav-mob');
-      sidenavMob.style.display = 'block';
+      if (!this.opened && this.firstTime) {
+        sidenavMob.style.display = 'block';
+        this.firstTime = false;
+      }
     }
     this.opened = !this.opened;
     this.expand = !this.expand;
